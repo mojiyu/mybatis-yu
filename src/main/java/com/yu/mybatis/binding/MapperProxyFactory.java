75e8ff970548efb4c137adc2b5a70f0fd2e9603e
bibi <bibi@qunhequnhe.com>
2022-06-08 19:28:41 +0800

    代理类和工厂

---
+++ src/main/java/com/yu/mybatis/binding/MapperProxyFactory.java
@@ -0,0 +1,23 @@
+package com.yu.mybatis.binding;
+
+import java.lang.reflect.Proxy;
+import java.util.Map;
+
+/**
+ * 代理类工厂
+ * @param <T>
+ */
+public class MapperProxyFactory<T> {
+
+    private final Class<T> mapperInterface;
+
+    public MapperProxyFactory(Class<T> mapperInterface) {
+        this.mapperInterface = mapperInterface;
+    }
+
+    public T newInstance(Map<String, String> sqlSession) {
+        final MapperProxy<T> mapperProxy = new MapperProxy<>(sqlSession, mapperInterface);
+        return (T) Proxy.newProxyInstance(mapperInterface.getClassLoader(), new Class[]{mapperInterface}, mapperProxy);
+    }
+
+}
